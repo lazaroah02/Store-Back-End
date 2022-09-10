a2fb2ebe71d2b9697f5e9573954c7c860e209c80
lazaroah <lazaroah02@gmail.com>
2022-09-10 10:03:54 -0400

    anonymus users cant accecc to user-profile

--- user_profile/views.py
+++ user_profile/views.py
@@ -3,11 +3,12 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import UserProfile
 from .serializer import UserProfileSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class UserProfile_api(APIView):
     serializer_class = UserProfileSerializer
-    
+    permission_classes = [IsAuthenticated]
     #get the user profile of the authenticated user
     def get(self, request): 
         try:
@@ -16,20 +17,20 @@ class UserProfile_api(APIView):
                 {"phone":profile.phone,"address":profile.address,"description":profile.description,"photo":profile.photo.url},
                 status=status.HTTP_200_OK)
         except:
-            return Response({"message":"No tienes perfil creado"})
+            return Response({"message":"No tienes perfil creado"})   
         
     
     #post to create a new user profile
     def post(self, request):
-        try:
-            serializer = self.serializer_class(data = request.data)
-            if serializer.is_valid() and serializer.validated_data["user"] == request.user:
-                serializer.save()
-                return Response(status = status.HTTP_200_OK)
-            else:
-                return Response(status = status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response(status = status.HTTP_400_BAD_REQUEST)    
+            try:
+                serializer = self.serializer_class(data = request.data)
+                if serializer.is_valid() and serializer.validated_data["user"] == request.user:
+                    serializer.save()
+                    return Response(status = status.HTTP_200_OK)
+                else:
+                    return Response(status = status.HTTP_400_BAD_REQUEST)
+            except:
+                return Response(status = status.HTTP_400_BAD_REQUEST)    
         
     #put to update the user profile
     def put(self, request):
